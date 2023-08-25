1cc0cf6b2a4a3ebaeb2b89efa446333c79f0d619
Rafeek Alas <rafeekalas.official@gmail.com>
2023-08-25 12:39:15 +0530

    Stage 2 - Data Validation added

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from src.mlproject import logger
 from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started  <<<<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<<<\n\nX=====================")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started  <<<<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<<<\n\nX=====================")
 except Exception as e:
